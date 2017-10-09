dcd5b9653b92520f0b195be97830397d27334ff5
Henrique Soares da Silva <henrique.silva533@etec.sp.gov.br>
2017-10-09 17:06:57 -0300

    Add project files.

---
+++ TrabalhoDeAS/Login.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrabalhoDeAS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Logar(object sender, RoutedEventArgs e)
+        {
+            if (txtId.Text == "caixa")
+            {
+                this.Hide();
+                new TelaPrincipal().ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("ID não encontrado!", "Erro!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+                
+        }
+    }
+}
